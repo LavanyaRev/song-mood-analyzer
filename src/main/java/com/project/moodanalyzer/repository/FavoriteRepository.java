67c00c1cc39eedb8bb02c5280ea0d74ab9c03bec
Lavanya Revankar <lavanyarevakar@gmail.com>
2025-10-05 14:57:14 +0530

    Initial commit - MoodAnalyzer backend

--- src/main/java/com/project/moodanalyzer/repository/FavoriteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.moodanalyzer.repository;
-
-import com.project.moodanalyzer.entity.Favorite;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findByUserId(Long userId);
-}
